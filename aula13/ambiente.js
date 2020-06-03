57774c903a444679494781ce4b9da1a00e72178c
Helena <helena.mkopp@gmail.com>
2020-06-03 17:48:24 -0300

    Dia 03/06

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+
+
+
+/*var c = 1
+while (c <= 4) {
+    console.log(`Passo ${c}`)
+    c++ // mesma coisa que c = c + 1 
+}
+*/
+
+
+
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')*/
+
