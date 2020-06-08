d88c8d8b3deeb1b2312c68a74110771c192a957f
Helena <helena.mkopp@gmail.com>
2020-06-08 15:25:48 -0300

    novo commit

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -6,9 +6,9 @@ function contar() {
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = 'Impossível contar'
-        //window.alert('[ERRO] Faltam dados!')
+        window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br/>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
@@ -19,12 +19,12 @@ function contar() {
         if (i < f ) {
            // contagem crescente
             for(let c = i; c <= f; c += p) {
-                res.innerHTML += `${c} \u{1F449}`
+                res.innerHTML += ` ${c} \u{1F449} `
             }
         } else {
             // contagem regressiva
             for(let c = i; c >= f; c -= p) {
-                res.innerHTML += `${c} \u{1F449}`
+                res.innerHTML += ` ${c} \u{1F449} `
             }
         }
         res.innerHTML += `\u{1F3C1}` 
---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9, 3]
+let pos = num.indexOf(2)
+// num.push(14) // .push significa que o JS vai adicionar ao array na ultima posição 
+// num[3] = 10 // aqui você pode constrolar em qual lugar do array voce quer adicionar e aqui ele substituiu o 9 pelo 10
+// num.length // atributo que serve para contar o tamanho do array
+// num.sort() // organiza em ordem crescente 
+console.log(`Nosso vetor é o ${num}`)
+console.log(`${num.length}`)
+console.log(`O valor 8 esta na posição ${pos}`)
